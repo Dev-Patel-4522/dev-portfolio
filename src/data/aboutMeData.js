7669b0517bb627678a3ef1b781753fe810d30062
Dev-Patel-4522 <devpatel7524@gmail.com>
2023-07-19 01:23:29 +0530

    Create a React app and publish it to GitHub Pages

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -2,12 +2,12 @@ export const aboutMeData = [
   {
     id: 1,
     intro: "Introduction",
-    bio: "I am Dev Patel and pursuing B.Tech degree in Computer Engineering. I am a fresher with knowledge of ReactJS, HTML, CSS and JavaScript. I keep a good interest in Python, Internet of Things (IoT) and Tech News. I am having deep interest in Current Affairs, Geo-Politics, and Foreign Relations of Bhārat Gaṇarājya with other respective Countries or States.",
+    bio: "I am Dev Patel and pursuing B.Tech Degree in Computer Engineering. I am a Fresher with knowledge of HTML, CSS, ReactJS and JavaScript. I keep a good interest in Python, Internet of Things (IoT) and Tech News. I am having deep interest in Current Affairs, Geo-Politics, and Foreign Relations of Bhārat Gaṇarājya with other respective Countries or States.",
   },
   {
     id: 2,
     intro: "My Internships",
-    bio: "I have done Internships in Front-End development with ReactJS, HTML, CSS, and JS (January 2023 - May 2023) & (June 2022 - July 2022). I Have done Internship in Linux Red Hat (January 2019 - April 2019) and Computer Hardware Maintenance & Troubleshooting (February 2018 - April 2018).",
+    bio: "I have done Internship(s) at SoftCoding Solutions in Front-End development with HTML, CSS, ReactJS and JS (January 2023 - May 2023) & HTML, CSS and JS (June 2022 - July 2022). I Have done Internship in Linux Red Hat (January 2019 - April 2019) and Computer Hardware Maintenance & Troubleshooting (February 2018 - April 2018).",
   },
   {
     id: 3,
@@ -22,6 +22,6 @@ export const aboutMeData = [
   {
     id: 5,
     intro: "Equipments",
-    bio: "I use to code Dell G3 15 Laptop, Dell Wireless Mouse WM118 and INFINITY GLIDE 510 On-Ear Headphone.",
+    bio: "I use to codeing Dell G3 15 Laptop, Dell Wireless Mouse WM118 and INFINITY GLIDE 510 On-Ear Headphone.",
   },
 ];
