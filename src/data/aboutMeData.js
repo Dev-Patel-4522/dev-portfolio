e79c483b17b077c346f5b378b18e13ec7e959112
Dev-Patel-4522 <devpatel7524@gmail.com>
2024-02-12 16:44:21 +0530

    upgraded

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -2,17 +2,17 @@ export const aboutMeData = [
   {
     id: 1,
     intro: "Introduction",
-    bio: "I am Dev Patel and pursuing B.Tech Degree in Computer Engineering. I am a Fresher with knowledge of HTML, CSS, ReactJS and JavaScript. I keep a good interest in Python, Internet of Things (IoT) and Tech News. I am having deep interest in Current Affairs, Geo-Politics, and Foreign Relations of Bhārat Gaṇarājya with other respective Countries or States.",
+    bio: "I am Dev Patel and a Fresher with knowledge of HTML, CSS, ReactJS and JavaScript. I keep a good interest in Python, Internet of Things (IoT) and Tech News. I am having deep interest in Current Affairs, Geo-Politics, and Foreign Relations of Bhārat Gaṇarājya with other respective Countries or States.",
   },
   {
     id: 2,
     intro: "My Internships",
-    bio: "I have done Internship(s) at SoftCoding Solutions in Front-End Development with HTML, CSS, ReactJS and JS (January 2023 - May 2023) & HTML, CSS and JS (June 2022 - July 2022). I Have done Internship in Linux Red Hat (January 2019 - April 2019) and Computer Hardware Maintenance & Troubleshooting (February 2018 - April 2018).",
+    bio: "I have done Internship(s) at SoftCoding Solutions in Frontend Development with HTML, CSS, ReactJS and JS (January 2023 - May 2023) & HTML, CSS and JS (June 2022 - July 2022). I Have done Internship in Linux Red Hat (January 2019 - April 2019) and Computer Hardware Maintenance & Troubleshooting (February 2018 - April 2018).",
   },
   {
     id: 3,
     intro: "Bachelor of Technology (Diploma to Degree)",
-    bio: "Bachelor of Technology (Diploma to Degree) in Computer Engineering from Silver Oak College of Engineering and Technology (Silver Oak University Affiliation) with SPI:- 8.35(Seventh Semester).",
+    bio: "Bachelor of Technology (Diploma to Degree) in Computer Engineering from Silver Oak College of Engineering and Technology (Silver Oak University Affiliation) with CGPA:- 8.58.",
   },
   {
     id: 4,
